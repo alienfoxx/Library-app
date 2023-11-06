0f0faafc06c3c706a301eb9557b4f8d3b7548954
alienfoxx <alienfoxx@gmail.com>
2023-11-06 10:42:50 +0100

    finshed the first edition of the library

--- main.js
+++ main.js
@@ -13,33 +13,79 @@ function Book(title, author , pages, read ) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.notRead =notRead;
+  this.notRead = notRead;
 }
+
+Book.prototype.toggleRead = function() {
+  this.read = !this.read;
+}
+function toggleRead(index){
+  myLibrary[index].toggleRead();
+  render();
+}
+function render(){
+  let libraryBook = document.querySelector('#library');
+  libraryBook.innerHTML = "";
+  for (let i = 0; i < myLibrary.length; i++){
+    let book = myLibrary[i];
+    let bookEL = document.createElement("div");
+    bookEL.setAttribute("class", "book-card");
+    bookEL.innerHTML = `
+    <div class="card-header>
+      <h3 class="title">${book.title}</h3>
+      <h5 class="author"> By ${book.author}</h5>
+    </div>
+    <div class="card-body">
+      <p> ${book.pages}  pages</p>
+      <p class="read-status"> ${book.read ? "Read" : "Not Read Yet"} </p>
+      
+    </div>
+    <button class="remove-btn" onclick="removeBook(${i})">Remove Book</button>
+    <button class ="toggle-read-btn" onclick="toggleRead(${i})"> Toggle Read</button>`;
+    libraryBook.appendChild(bookEL)
+  }
+
+}
+
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+  render()
+}
+
+
 function addBookToLibrary() {
-  
-}
-
-
-
-addBook.addEventListener("click", function(){
-  let bookForms = document.querySelector('#bookForm');
-   let savBook = document.querySelector('#saveBook')
-   let readForm= document.querySelector(".read-form")
-  bookForms.style.display ="grid";
-  readForm.style.display="flex";
-  savBook.style.cssText = "display: flex; align-items:center";
-
-});
- savBook.addEventListener("click", function(event){
-
   let title = document.querySelector('#title').value;
-let author = document.querySelector('#autor').value;
+  let author = document.querySelector('#autor').value;
 let pages = document.querySelector('#pages').value;
 let read = document.querySelector('#read-it').checked;
 let notRead = document.querySelector('#no-read').checked;
 
 let newBook = new Book(title, author, pages, read, notRead);
   myLibrary.push(newBook);
-  console.log(myLibrary);
- 
- })
+       setTimeout(function() {
+    document.querySelector('#title').value = "";
+    document.querySelector('#autor').value = "";
+    document.querySelector('#pages').value = "";
+    document.querySelector('#read-it').checked = false;
+    document.querySelector('#no-read').checked = false;
+  }, 200);
+  
+  render()
+}
+
+
+
+
+addBook.addEventListener("click", function(){
+  let bookForms = document.querySelector('#bookForm');
+   let savBook = document.querySelector('#saveBook')
+   let readForm = document.querySelector(".read-form")
+  bookForms.style.display ="grid";
+  readForm.style.display="flex";
+  savBook.style.cssText = "display: flex; align-items:center";
+
+});
+ savBook.addEventListener("click", function(event){
+addBookToLibrary();
+  
+});
